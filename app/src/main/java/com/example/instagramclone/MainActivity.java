1fefa8438e941f99c06bf43a1e2948b2a10c77f0
Binod Kumar <vk055249@gmail.com>
2020-06-21 19:20:17 +0530

    SignUp and Login Parse

--- app/src/main/java/com/example/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -99,6 +100,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
            btnNxtActivity.setOnClickListener(new View.OnClickListener() {
                @Override
                public void onClick(View v) {
+
+                   Intent intent=new Intent(MainActivity.this,SignUpLoginActivity.class);
+                   startActivity(intent);
 
                }
            });
